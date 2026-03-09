e01de51b53fb4961979066c2ace61265c33e3e4b
Tusharkhadde <tusharkhadde06@gmail.com>
2026-03-09 12:29:04 +0530

    fix changes

--- Brainly-Backend/src/index.ts
+++ Brainly-Backend/src/index.ts
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URL as string)
     .catch((err) => console.log(err));
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL || ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:5175"],
+    origin: process.env.FRONTEND_URL || ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:5175", "https://second-brain-jade-xi.vercel.app"],
     credentials: true
 }));
 app.use(express.json());
