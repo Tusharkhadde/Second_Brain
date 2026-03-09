3682d11e748fd3d70284d395a055a0dd6b9239ad
Tusharkhadde <tusharkhadde06@gmail.com>
2026-03-09 09:24:49 +0530

    Update URLs

--- Brainly-Backend/src/index.ts
+++ Brainly-Backend/src/index.ts
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URL as string)
     .catch((err) => console.log(err));
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+    origin: process.env.FRONTEND_URL || ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:5175"],
     credentials: true
 }));
 app.use(express.json());
